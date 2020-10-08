74f6dd1e50f1e86f88d9380f65de5c4e839ac126
S <sumanth.s-@hpe.com>
2020-10-08 12:48:42 +0530

    Updated

--- Version 2/Cards/Audit_run.py
+++ Version 2/Cards/Audit_run.py
@@ -1,23 +1,59 @@
 import sys,os
 from datetime import datetime
 import time
+from optparse import OptionParser,OptionGroup
+import logging
 
-now = datetime.now()
-print("now =", now)
-date = now.strftime("%m-%d-%Y"+'_'+"%H-%M-%S")
-print("date and time =", date)	
+help_message = "\n -i, --input [input_file_path] \n"
 
-if len(sys.argv)>1:
-    if sys.argv[1]=='-help':
-        print("Format:  py <scrpit to run>.py -input <input file path>")
-    elif sys.argv[1]=='-input' and sys.argv[2]!='':
-        os.system('cmd /c "robot -v input_file:"'+sys.argv[2]+'" -v day:"'+date+'" audit_card.robot"')
-        time.sleep(10)
-        os.system('cmd /c "robot -v day:"'+date+'" fetch_file.robot"')
-        time.sleep(10)
-        os.system('cmd /c "py Python_files\\summary.py "'+sys.argv[2]+'" "'+date)        
+def get_date():
+    now = datetime.now()
+    #print("now =", now)
+    date = now.strftime("%m-%d-%Y"+'_'+"%H-%M-%S")
+    print("date and time =", date)
+    return date	
+
+def main():
+    date = get_date()
+    os.system('cmd /c "robot -v input_file:"'+sys.argv[2]+'" -v day:"'+date+'" audit_card.robot"')
+    time.sleep(10)
+    os.system('cmd /c "robot -v day:"'+date+'" fetch_file.robot"')
+    time.sleep(10)
+    os.system('cmd /c "py Python_files\\summary.py "'+sys.argv[2]+'" "'+date)        
+           
+        
+def add_options (parser):
+    """
+    Method that defines the options(prameters) for the script.
+    """
+    parser.add_option("-h","--help",  help = help_message , action="store_true",default = False)
+    parser.add_option("-i","--input", help = "input file path", default = None)
+
+    
+def initialize():
+    add_options(parser)                                                                 
+    (options, args) = parser.parse_args()
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler("debug.log",mode='w')
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(logging.Formatter('%(levelname)s : %(message)s'))
+    """console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(logging.Formatter('%(message)s'))"""
+    logger.addHandler(file_handler)
+    #logger.addHandler(console_handler)
+    return options , logger
+
+parser = OptionParser(add_help_option=False)
+options , logger = initialize()
+
+if __name__=='__main__':
+    if options.help:
+        print(help_message)
+        sys.exit()
+    elif options.input != None:
+        main()
     else:
-        print("Enter -help for input format")       
-else:
-    print("Enter -help for input format")
+        print("-h, --help for help message")
         
